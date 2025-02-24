5cc45e2261654deeb322400453f715b201b2feea
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:27:26 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TableExportAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+    }
+}
