903f0b240902f7504f8724f359a80e1b7103b331
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:26:28 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TableExportAPI.Data;
+using TableExportAPI.Models;
+
+namespace TableExportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+    }
+}
