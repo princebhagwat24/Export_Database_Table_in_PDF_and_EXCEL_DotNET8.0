e77b69dd25cc1aebb64fbad6b9dbe596991bf043
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:26:55 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableExportAPI.Models;
+
+
+namespace TableExportAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
